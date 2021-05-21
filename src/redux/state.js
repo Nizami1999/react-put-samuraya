155ff55483b347f9cae92fe1b66cdc4f91c35858
Nizami <nizami.alimamedov@mail.ru>
2021-05-21 15:08:22 +0400

    37. store, bind

--- src/redux/state.js
+++ src/redux/state.js
@@ -1,51 +1,66 @@
-import { rerenderEntireTree } from "../render";
+let store = {
 
-let state = {
+    _state : {
 
-    dialogsPage : {
-
-        dialogs : [
-            { id: "1", name: "Hebib" },
-            { id: "2", name: "Ferid" },
-            { id: "3", name: "Perviz" },
-            { id: "4", name: "Senan" }
-        ],
-
-        messages : [
-            { id: "1", message: "Pubg?" },
-            { id: "2", message: "React?" },
-            { id: "3", message: "Umbuluw?" },
-            { id: "4", message: "Zdarova?" }
-        ],
+        dialogsPage: {
+    
+            dialogs: [
+                { id: "1", name: "Hebib" },
+                { id: "2", name: "Ferid" },
+                { id: "3", name: "Perviz" },
+                { id: "4", name: "Senan" }
+            ],
+    
+            messages: [
+                { id: "1", message: "Pubg?" },
+                { id: "2", message: "React?" },
+                { id: "3", message: "Umbuluw?" },
+                { id: "4", message: "Zdarova?" }
+            ],
+        },
+    
+        profilePage: {
+    
+            posts: [
+                { userName: "NizamiReact01", photo: "https://www.meme-arsenal.com/memes/b5d2ec8e1ffa887b239fb66a8653dfe6.jpg", likes: "83", postMessage: "Hello my name is Nizami" },
+                { userName: "NizamiReact01", photo: "https://www.meme-arsenal.com/memes/b5d2ec8e1ffa887b239fb66a8653dfe6.jpg", likes: "90", postMessage: "Hello my name is" },
+                { userName: "NizamiReact01", photo: "https://www.meme-arsenal.com/memes/b5d2ec8e1ffa887b239fb66a8653dfe6.jpg", likes: "112", postMessage: "Hello my name" },
+                { userName: "NizamiReact01", photo: "https://www.meme-arsenal.com/memes/b5d2ec8e1ffa887b239fb66a8653dfe6.jpg", likes: "225", postMessage: "Hello my" }
+            ],
+            newPostText: ""
+        }
     },
 
-    profilePage : {
+    getState(){
+        return this._state;
+    },
 
-        posts : [
-            { userName: "NizamiReact01", photo: "https://www.meme-arsenal.com/memes/b5d2ec8e1ffa887b239fb66a8653dfe6.jpg", likes: "83", postMessage: "Hello my name is Nizami" },
-            { userName: "NizamiReact01", photo: "https://www.meme-arsenal.com/memes/b5d2ec8e1ffa887b239fb66a8653dfe6.jpg", likes: "90", postMessage: "Hello my name is" },
-            { userName: "NizamiReact01", photo: "https://www.meme-arsenal.com/memes/b5d2ec8e1ffa887b239fb66a8653dfe6.jpg", likes: "112", postMessage: "Hello my name" },
-            { userName: "NizamiReact01", photo: "https://www.meme-arsenal.com/memes/b5d2ec8e1ffa887b239fb66a8653dfe6.jpg", likes: "225", postMessage: "Hello my" }
-        ],
-        newPostText: ""
+    _callSubscriber() {
+    console.log("State changed");
+    },
+
+    addPost() {
+        let newPost = {
+            userName: "NizamiReact01",
+            photo: "https://www.meme-arsenal.com/memes/b5d2ec8e1ffa887b239fb66a8653dfe6.jpg",
+            likes: "0",
+            postMessage: this._state.profilePage.newPostText
+        }
+        this._state.profilePage.posts.push(newPost);
+        this._state.profilePage.newPostText = "";
+        this._callSubscriber(this._state);
+    },
+
+    updateNewPostText(newText) {
+        this._state.profilePage.newPostText = newText;
+        this._callSubscriber(this._state);
+    },
+    
+    subscribe(observer) {
+        this._callSubscriber = observer;
     }
+
 }
 
-export let addPost = () =>{
-    let newPost = {
-        userName: "NizamiReact01",
-        photo: "https://www.meme-arsenal.com/memes/b5d2ec8e1ffa887b239fb66a8653dfe6.jpg",
-        likes: "0",
-        postMessage: state.profilePage.newPostText 
-    }
-    state.profilePage.posts.push(newPost);
-    state.profilePage.newPostText="";
-    rerenderEntireTree(state);
-}
-
-export let updateNewPostText = (newText) =>{
-    state.profilePage.newPostText = newText;
-    rerenderEntireTree(state);
-}
-
-export default state;
+window.store = store;
+export default store;
